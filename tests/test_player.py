aa888535335907b3f4ae7827ec62a7bf9c0e4188
Hosokawa Junya <hosokawa@morijyobi.ac.jp>
2024-10-28 16:30:53 +0900

    modified tests/test_player.py mockerの記述部分

--- tests/test_player.py
+++ tests/test_player.py
@@ -5,29 +5,29 @@ from unittest.mock import patch
 このコードをPytestでテストするために、標準入力をモックする必要があります。
 '''
 
-def test_pon_guu():
+def test_pon_guu(mocker):
 # return_value
 # モックが呼び出された際に返す値を設定
-    with patch('builtins.input', return_value='1'):
-        assert player_pon() == 'グー'
+    mocker.patch('builtins.input', return_value='1')
+    assert player_pon() == 'グー'
 
-def test_pon_choki():
+def test_pon_choki(mocker):
 # return_value
 # モックが呼び出された際に返す値を設定
-    with patch('builtins.input', return_value='2'):
-        assert player_pon() == 'チョキ'
+    mocker.patch('builtins.input', return_value='2')
+    assert player_pon() == 'チョキ'
 
-def test_pon_paa():
+def test_pon_paa(mocker):
 # return_value
 # モックが呼び出された際に返す値を設定
-    with patch('builtins.input', return_value='3'):
-        assert player_pon() == 'パー'
+    mocker.patch('builtins.input', return_value='3')
+    assert player_pon() == 'パー'
 
-def test_pon_invalid_input():
+def test_pon_invalid_input(mocker):
 # side_effect
 # モックが呼ばれた際に呼び出される関数、イテラブル、もしくは発生させる例外 (クラスまたはインスタンス) を設定
-    with patch('builtins.input', side_effect=['4', '2']):
-        assert player_pon() == 'チョキ'
+    mocker.patch('builtins.input', side_effect=['4', '2'])
+    assert player_pon() == 'チョキ'
 
 if __name__ == "__main__":
     pytest.main()
